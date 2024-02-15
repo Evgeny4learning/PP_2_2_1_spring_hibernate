615e641c6162a55e4a761b1fd3ee03518550e4d2
Evgeny4learning <fulln1ghtt@yandex.ru>
2024-02-15 14:56:15 +0300

    added entity Car and onetoone relation

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,19 +16,30 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+//      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+//      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+//      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+      userService.add(new User("Kalh", "Gogov", "infer1@mail.ru"));
+      userService.add(new User("Fafnir", "Nagov", "tower1@mail.ru"));
+      userService.add(new User("Ignat", "Besov", "infer2@mail.ru"));
+      userService.add(new Car("lada priora",112));
+      userService.add(new Car("opasniy devyatos",44));
+      userService.add(new Car("niva",636));
 
       List<User> users = userService.listUsers();
+
       for (User user : users) {
          System.out.println("Id = "+user.getId());
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+user.getCar().getModel());
          System.out.println();
       }
+
+      System.out.println(userService.getUserByCar().getFirstName());
 
       context.close();
    }
